2206d86f74c9ba84b757b5bd78f16648dd25dfb2
Akash Kumar <84145371+Akash02032002@users.noreply.github.com>
2023-09-09 20:33:03 +0530

    Fisrt Commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore, collection} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const moviesRef = collection(db, "movies");
+export const reviewsRef = collection(db, "reviews");
+export const usersRef = collection(db, "users");
+
+export default app;
